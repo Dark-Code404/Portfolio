e4fda5ed437b252bf48bf543cc4b135f4bc37409
Dark-Code404 <luiyunish@gmail.com>
2024-06-04 12:59:14 +0545

    updated

--- Portfolio_website/settings.py
+++ Portfolio_website/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,10 +130,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-if not DEBUG:
-    STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+
+if not DEBUG:   
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
--- build.sh
+++ build.sh
@@ -7,5 +7,7 @@ pip install -r requirement.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
+python manage.py makemigrations
+
 # Apply any outstanding database migrations
 python manage.py migrate
