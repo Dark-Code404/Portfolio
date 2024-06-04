8c6785be2451c83fcbc61b9310684cce4312346f
Dark-Code404 <luiyunish@gmail.com>
2024-06-04 17:23:50 +0545

    updated

--- Portfolio_website/settings.py
+++ Portfolio_website/settings.py
@@ -137,7 +137,7 @@ if not DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
 
 
 
