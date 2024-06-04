a6a7b084212a584cfa804285624f8be34c5bb2c4
Dark-Code404 <luiyunish@gmail.com>
2024-06-04 11:26:22 +0545

    added build file

--- Portfolio_website/urls.py
+++ Portfolio_website/urls.py
@@ -3,7 +3,16 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Main_app.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
+
+if not DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
