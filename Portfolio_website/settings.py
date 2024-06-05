aafd762bc179db6c160f622d1508b120182b1263
Dark-Code404 <luiyunish@gmail.com>
2024-06-05 15:34:50 +0545

    updated settings.py

--- Portfolio_website/settings.py
+++ Portfolio_website/settings.py
@@ -70,6 +70,14 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+
+              'loaders': [
+            ('django.template.loaders.cached.Loader', [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+                'path.to.custom.Loader',
+            ]),
+        ],
         },
     },
 ]
