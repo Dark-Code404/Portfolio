5aa27f4049fb60dd5e1dd27d4cdbd48ad1285ec7
Dark-Code404 <luiyunish@gmail.com>
2024-07-30 19:15:45 +0545

    first commit

--- build_files.sh
+++ build_files.sh
@@ -2,14 +2,14 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-python3.9 -m ensurepip 
 
-python3.9 -m pip install -r requirements.txt
+
+python -m pip install -r requirements.txt
 
 # Convert static asset files
-python3.9 manage.py collectstatic
+python manage.py collectstatic
 
-python3.9 manage.py makemigrations
+python manage.py makemigrations
 
 # Apply any outstanding database migrations
-python3.9 manage.py migrate
+python manage.py migrate
