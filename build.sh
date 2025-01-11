688a2473713de04e7d8f2ad4cba4b18a2a825cc2
Dark-Code404 <luiyunish@gmail.com>
2025-01-12 01:50:44 +0545

    optimized my resume

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 
 set -o errexit
 
+set -e  # Exit on any error
+
+# Set up a virtual environment
+python3 -m venv /opt/venv
+source /opt/venv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
